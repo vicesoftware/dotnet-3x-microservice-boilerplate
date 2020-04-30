926114b7127c14f3a59fea19e40b52c784198c6c
Ryan Vice <ryan@vicesoftware.com>
2020-04-30 13:01:46 -0500

    Upgraded to 3.1 dotnet and got build for docker-compose.yml working on port 5000

--- api/Program.cs
+++ api/Program.cs
@@ -1,11 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace api
@@ -14,12 +13,14 @@ namespace api
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseUrls("http://0.0.0.0:5000")
-                .UseStartup<Startup>();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
--- tests/ValuesControllerSpec.cs
+++ tests/ValuesControllerSpec.cs
@@ -2,14 +2,17 @@ using api.Controllers;
 using Xunit;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Linq;
+using api;
+using Microsoft.Extensions.Logging.Abstractions;
 
 namespace tests {
     public class ValuesControllerSpec{
-        private readonly ValuesController sut;
+        private readonly WeatherForecastController sut;
 
         public ValuesControllerSpec()
         {
-            sut = new ValuesController();
+            sut = new WeatherForecastController(new NullLogger<WeatherForecastController>());
         }
 
         [Fact]
@@ -23,8 +26,8 @@ namespace tests {
         public void get_all_values_returns_3_items()
         {
             var result = sut.Get().Result as OkObjectResult;
-            var items = Assert.IsType<string[]>(result.Value);
-            Assert.Equal(3, items.Length);
+            var items = Assert.IsType<WeatherForecast[]>(result.Value);
+            Assert.Equal(5, items.Count());
         }
     }
 }

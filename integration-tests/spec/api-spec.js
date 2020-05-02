96abaf820a8e54759f8bfe84b14cc9a14b03610c
Ryan Vice <ryan@vicesoftware.com>
2020-05-02 09:39:07 -0500

    Got integration tests working in docker.

--- integration-tests/spec/api-spec.js
+++ integration-tests/spec/api-spec.js
@@ -1,38 +1,18 @@
 const axios = require('axios');
 
-describe('ACME API test suite', () => {
-    describe('Some smoke test...', () => {
-        it('should succeed!', () => {
-            expect(true).toBe(true);
-        })
-    })
-
-    describe('When calling GET /api/values', () =>{
-        const url = 'http://api:5000/api/values';
+describe('Vice Software API test suite', () =>
+    describe('When calling GET /weatherforecast', () =>{
+        const url = 'http://api:5000/weatherforecast';
 
         it('should return OK', async () => {
+            console.log(url)
             const response = await axios.get(url);
             expect(response.status).toBe(200);
         })
 
         it('should return a description', async ()=> {
             const response = await axios.get(url);
-            expect(response.data).toEqual([ 'value1', 'value2', 'values3' ]);
-        })
-        
-    })
-
-    describe('When calling GET /api/values/1', () => {
-        const baseUrl = 'http://api:5000/api';
-
-        it('should return OK.', async () => {
-            const response = await axios.get(baseUrl + '/values/1');
-            expect(response.status).toBe(200);
-        })
-
-        it('should return a single value "value<id>"', async () => {
-            const response = await axios.get(baseUrl + '/values/1');
-            expect(response.data).toBe("value1");
+            expect(response.data.length).toBe(5);
         })
     })
 })
